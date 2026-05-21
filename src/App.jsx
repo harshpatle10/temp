120d3bfd099b728a30a1487e83262a7d73e9a505
harsh patle <harshpatle99999@gmail.com>
2026-05-21 13:22:41 +0530

    Initial commit - HomeLay India real estate website

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeLayIndiaWebsite from './HomeLayIndiaWebsite'
+
+const App = () => {
+  return (
+    <div>
+      <HomeLayIndiaWebsite/>
+    </div>
+  )
+}
+
+export default App
