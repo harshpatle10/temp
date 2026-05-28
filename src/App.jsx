65c4906836a4d2fd2806a80db0efec4bec5bd09b
harsh patle <harshpatle99999@gmail.com>
2026-05-28 17:54:00 +0530

    add new feature in feauture-ui origin

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import BloodSync from './Bloodsync'
 const App = () => {
   return (
     <div>
+      maine feature add kar liya hu 
+      maine feature add kar liya hu 
+      maine feature add kar liya hu 
+      maine feature add kar liya hu 
+      maine feature add kar liya hu 
+      maine feature add kar liya hu 
+
+      Lorem ipsum dolor sit amet, conseFctetur adipisicing elit. Eius corporis reiciendis placeat odio! Minus, nobis in cupiditate autem quam perspiciatis dolorum quos praesentium iusto at dolor, placeat odio eos porro!
       {/* <HomeLayIndiaWebsite/> */}
       <BloodSync/>
     </div>
